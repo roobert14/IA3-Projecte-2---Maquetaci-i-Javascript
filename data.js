a7ede416a74a1730372cfb32f024201608afb730
Robert Ramirez De Matos <ramirezmatosrobert@gmail.com>
2026-03-26 16:14:21 +0100

    sss

---
+++ data.js
@@ -0,0 +1,26 @@
+export const destinacionsDestacades = [
+  {
+    id: "australia",
+    nom: "Australia",
+    imatgeUrl: "/home/Imagenes/hotel1.png",
+    propietatsCount: 2246,
+  },
+  {
+    id: "japan",
+    nom: "Japan",
+    imatgeUrl: "/images/destinacions/japan.jpg",
+    propietatsCount: 1278,
+  },
+  {
+    id: "new-zealand",
+    nom: "New Zealand",
+    imatgeUrl: "/images/destinacions/new-zealand.jpg",
+    propietatsCount: 480,
+  },
+  {
+    id: "greece",
+    nom: "Greece",
+    imatgeUrl: "/images/destinacions/greece.jpg",
+    propietatsCount: 320,
+  },
+];
